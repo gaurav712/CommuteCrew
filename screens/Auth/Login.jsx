f8df4e441f08020c94827804c20c8881ad3a402d
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-05-09 12:15:20 +0530

    add simple loader to login screen

--- screens/Auth/Login.jsx
+++ screens/Auth/Login.jsx
@@ -1,11 +1,45 @@
-import {Text, View} from 'react-native';
+import {useEffect, useState} from 'react';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 
-const Login = () => {
+const Login = ({navigation}) => {
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      setAuthenticated(true);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (loading === false && authenticated) {
+      navigation.replace('HomeStack');
+    }
+  }, [authenticated]);
+
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <>
+        {loading ? <ActivityIndicator size={50} color={'#000000'} /> : null}
+        <Text style={styles.text}>Logging you in ...</Text>
+      </>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 20,
+    fontFamily: 'Raleway-Regular',
+  },
+});
 
 export default Login;
