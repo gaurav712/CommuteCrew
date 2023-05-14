d20893fa3885d8ae26677f26e73bc804816fb910
pushparaj21 <singhpushparaj31@gmail.com>
2023-05-14 16:54:27 +0530

    make profile editable

--- screens/Home/ProfileScreen.jsx
+++ screens/Home/ProfileScreen.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   View,
@@ -9,134 +9,181 @@ import {
   Modal,
   Pressable,
   Alert,
+  TextInput,
+  ScrollView,
 } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 export default function ProfileScreen() {
   const [profilePic, setProfilePic] = useState(
     require('../../assets/avatar.png'),
   );
-  const [name, setName] = useState('Pushparaj singh');
-  const [email, setEmail] = useState('singhpushparaj31@gmail.com');
-  const [phone, setPhone] = useState('8840587225');
-  const [bio, setBio] = useState('');
+  const [profileD, setProfileD] = useState({
+    name: 'Pushparaj singh',
+    phone: '9838405840',
+    email: 'singhpushparaj',
+    vehicle: 'todo',
+    bio: '',
+    preferences: '',
+  });
   const [modalVisible, setModalVisible] = useState(false);
-  const [preferences, setPreferences] = useState('');
   const [target, setTarget] = useState('');
-  const [vehicle, setVehicle] = useState();
-  const kk = {
-    email: setEmail,
-    phone: setPhone,
+  const handleMobileChange = newdata => {
+    console.log(target, newdata);
+    setProfileD({
+      ...profileD, // Copy the existing state object
+      [target]: newdata, // Update the mobile number with the new value
+    });
   };
+
   const handleEditProfilePic = () => {
     // Implement logic for editing personal details
   };
 
   return (
-    <View style={styles.container}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>{target}</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
+    <ScrollView>
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>{target}</Text>
+              <TextInput
+                value={profileD[target]}
+                onChangeText={text => handleMobileChange(text, target)}
+              />
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}>
+                <Text style={styles.textStyle}>Okey</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text style={styles.name}>{profileD.name}</Text>
+            <TouchableOpacity
               onPress={() => {
-                kk[target](9838405840);
-                // const the = target.charAt(0).toUpperCase() + target.slice(1);
-                // console.log(the);
-                // `set${the}`('the');
-                setModalVisible(!modalVisible);
+                setTarget('name');
+                setModalVisible(true);
               }}>
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
+              <AntDesign
+                name="edit"
+                size={20}
+                color="#00B0FF"
+                style={{marginLeft: 5, paddingBottom: 10}}
+              />
+            </TouchableOpacity>
           </View>
-        </View>
-      </Modal>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}>
-        <Text style={styles.name}>{name}</Text>
 
-        <Image source={profilePic} style={styles.profilePic} />
-      </View>
-      <TouchableOpacity
-        onPress={handleEditProfilePic}
-        style={styles.editProfilePic}>
-        <Text style={styles.textClick}>Edit Profile Picture</Text>
-      </TouchableOpacity>
-      <View style={styles.line} />
-      {/* profile section start */}
-      <View style={styles.section}>
-        <Text style={styles.sectionHeader}>Verify your profile</Text>
-        {/* email test */}
+          <Image source={profilePic} style={styles.profilePic} />
+        </View>
         <TouchableOpacity
-          onPress={() => {
-            setTarget('email');
-            setModalVisible(true);
-          }}>
-          <View style={styles.sectionRow}>
-            <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
-            <View style={styles.sectionDataColoum}>
-              <Text style={styles.sectionText}>Comfirm email</Text>
-              {email && <Text style={styles.sectionText}>{email}</Text>}
-            </View>
-          </View>
+          onPress={handleEditProfilePic}
+          style={styles.editProfilePic}>
+          <Text style={styles.textClick}>Edit Profile Picture</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            setTarget('phone');
-            setModalVisible(true);
-          }}>
-          <View style={styles.sectionRow}>
-            <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
-            <View style={styles.sectionDataColoum}>
-              <Text style={styles.sectionText}>Comfirm phone number</Text>
-              {phone && <Text style={styles.sectionText}>{phone}</Text>}
+        <View style={styles.line} />
+        {/* profile section start */}
+        <View style={styles.section}>
+          <Text style={styles.sectionHeader}>Verify your profile</Text>
+          {/* email test */}
+          <TouchableOpacity
+            onPress={() => {
+              setTarget('email');
+              setModalVisible(true);
+            }}>
+            <View style={styles.sectionRow}>
+              <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
+              <View style={styles.sectionDataColoum}>
+                <Text style={styles.sectionText}>Comfirm email</Text>
+                {profileD.email && (
+                  <Text style={styles.sectionText}>{profileD.email}</Text>
+                )}
+              </View>
             </View>
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.line} />
-      <View style={styles.section}>
-        <Text style={styles.sectionHeader}>About you</Text>
-        <View style={styles.sectionRow}>
-          <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
-          <View style={styles.sectionDataColoum}>
-            <Text style={styles.sectionText}>Add a mini bio</Text>
-            {bio && <Text style={styles.sectionText}>{bio}</Text>}
-          </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              setTarget('phone');
+              setModalVisible(true);
+            }}>
+            <View style={styles.sectionRow}>
+              <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
+              <View style={styles.sectionDataColoum}>
+                <Text style={styles.sectionText}>Comfirm phone number</Text>
+                {profileD.phone && (
+                  <Text style={styles.sectionText}>{profileD.phone}</Text>
+                )}
+              </View>
+            </View>
+          </TouchableOpacity>
         </View>
-        <View style={styles.sectionRow}>
-          <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
-          <View style={styles.sectionDataColoum}>
-            <Text style={styles.sectionText}>Add my preferences</Text>
-            {preferences && (
-              <Text style={styles.sectionText}>{preferences}</Text>
-            )}
-          </View>
+        <View style={styles.line} />
+        <View style={styles.section}>
+          <Text style={styles.sectionHeader}>About you</Text>
+          <TouchableOpacity
+            onPress={() => {
+              setTarget('bio');
+              setModalVisible(true);
+            }}>
+            <View style={styles.sectionRow}>
+              <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
+              <View style={styles.sectionDataColoum}>
+                <Text style={styles.sectionText}>Add a mini bio</Text>
+                {profileD.bio && (
+                  <Text style={styles.sectionText}>{profileD.bio}</Text>
+                )}
+              </View>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              setTarget('preferences');
+              setModalVisible(true);
+            }}>
+            <View style={styles.sectionRow}>
+              <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
+              <View style={styles.sectionDataColoum}>
+                <Text style={styles.sectionText}>Add my preferences</Text>
+                {profileD.preferences && (
+                  <Text style={styles.sectionText}>{profileD.preferences}</Text>
+                )}
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.line} />
+        <View style={styles.section}>
+          <Text style={styles.sectionHeader}>Vehicle</Text>
+          <TouchableOpacity>
+            <View style={styles.sectionRow}>
+              <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
+              <View style={styles.sectionDataColoum}>
+                <Text style={styles.sectionText}>Add Vehicle</Text>
+                {profileD.vehicle && (
+                  <Text style={styles.sectionText}>{profileD.vehicle}</Text>
+                )}
+              </View>
+            </View>
+          </TouchableOpacity>
         </View>
       </View>
-      <View style={styles.line} />
-      <View style={styles.section}>
-        <Text style={styles.sectionHeader}>Vehicle</Text>
-        <View style={styles.sectionRow}>
-          <AntDesign name="pluscircleo" size={20} color="#00B0FF" />
-          <View style={styles.sectionDataColoum}>
-            <Text style={styles.sectionText}>Add Vehicle</Text>
-            {vehicle && <Text style={styles.sectionText}>{vehicle}</Text>}
-          </View>
-        </View>
-      </View>
-    </View>
+    </ScrollView>
   );
 }
 
