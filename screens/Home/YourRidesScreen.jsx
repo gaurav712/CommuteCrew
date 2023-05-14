7615ff05a4e7685319821a1f3cdbceb1f04eecc9
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-05-14 14:11:49 +0530

    change background

--- screens/Home/YourRidesScreen.jsx
+++ screens/Home/YourRidesScreen.jsx
@@ -1,10 +1,21 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 
-export default function YourRidesScreen() {
+const YourRidesScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={styles.container}>
       <Text>YourRidesScreen</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default YourRidesScreen;
